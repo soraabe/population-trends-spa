5cb44cfd3ed341e78e7a1401e050023fec8cd188
soraabe <abesora553@gmail.com>
2025-08-19 06:06:31 -0700

    feat(ui): 都道府県選択チェックボックスを追加 (#6)

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,19 +1,19 @@
 export type Prefecture = {
-    prefCode: number
-    prefName: string
-  }
+  prefCode: number
+  prefName: string
+}
 
-  export type PopulationItem = {
-    year: number
-    value: number
-  }
+export type PopulationItem = {
+  year: number
+  value: number
+}
 
-  export type PopulationSeries = {
-    label: "総人口" | "年少人口" | "生産年齢人口" | "老年人口"
-    data: PopulationItem[]
-  }
+export type PopulationSeries = {
+  label: "総人口" | "年少人口" | "生産年齢人口" | "老年人口"
+  data: PopulationItem[]
+}
 
-  export type PopulationResponse = {
-    boundaryYear: number
-    data: PopulationSeries[]
-  }
+export type PopulationResponse = {
+  boundaryYear: number
+  data: PopulationSeries[]
+}
